1fbe05a8f94eccaef743cce70bf688248c239083
Andrew Yatsura <andrewyazura203@gmail.com>
2022-03-26 21:08:27 +0200

    split export_user_data function

--- background-script.js
+++ background-script.js
@@ -1,35 +1,36 @@
 // this function is used by popup.js
-function export_user_data(params) {
-  browser.storage.local.clear();
-
+function export_current_user_data(params) {
   browser.tabs
     .query({ currentWindow: true, active: true })
     .then((tabs) => {
-      let profile_tab = tabs[0];
-      let username = get_username(profile_tab.url);
-
-      browser.tabs.executeScript(profile_tab.id, {
-        file: "/content-scripts/profile.js",
-      });
-
-      for (const type of params) {
-        if (type == "following" || type == "followers") {
-          browser.tabs
-            .create({ url: get_twitter_url(username, element) })
-            .then((tab) => {
-              browser.tabs.executeScript(tab.id, {
-                file: `/content-scripts/user-lists.js`,
-              });
-            })
-            .catch(console.error);
-        } else if (type == "retweets") {
-          console.warn("retweets are not implemented yet");
-        } else {
-          console.warn("unknown type:", type);
-        }
-      }
+      export_user_data(tabs[0], params);
     })
     .catch(console.error);
+}
+
+function export_user_data(user_tab, params) {
+  let username = get_username(user_tab.url);
+
+  browser.tabs.executeScript(user_tab.id, {
+    file: "/content-scripts/profile.js",
+  });
+
+  for (const type of params) {
+    if (type == "following" || type == "followers") {
+      browser.tabs
+        .create({ url: get_twitter_url(username, type) })
+        .then((tab) => {
+          browser.tabs.executeScript(tab.id, {
+            file: `/content-scripts/user-lists.js`,
+          });
+        })
+        .catch(console.error);
+    } else if (type == "retweets") {
+      console.warn("retweets are not implemented yet");
+    } else {
+      console.warn("unknown type:", type);
+    }
+  }
 }
 
 function get_twitter_url(username, path) {
